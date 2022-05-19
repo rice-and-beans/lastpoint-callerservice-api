2990c324fa0fddabdb8c1640077b73557cc457f6
Breno Augusto Comandolli <brenoaugustocomandolli@gmail.com>
2022-05-18 21:54:41 -0300

    #0005 - Padrão de estrutura
    +Falta adicionar template method
    +Change of responsability

--- src/controllers/qrcodeController.js
+++
@@ -1,8 +0,0 @@
-const qrcodeService = require('../services/qrcodeService.js');
-const qrcodeConsts = require('../constants/qrcodeConts.js');
-
-exports.post = async (req, res) =>{
-    const qrCode = await qrcodeService.criarQRCode();
-    res.type(qrcodeConsts.TIPO_IMAGE);
-    qrCode.pipe(res);
-};
--- src/routers/qrcodeRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const qrcodeController = require('../controllers/qrcodeController.js');
-
-router.post('/', qrcodeController.post);
-
-module.exports = router;
